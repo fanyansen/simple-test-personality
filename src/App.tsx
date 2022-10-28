5061ef313727ca62f0a7172dca603fc2198ab916
Fan Yansen <fan.yansen@onebyone.com.sg>
2022-10-28 21:15:48 +0700

    Finishing Simple Test Personality Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Grid from "@mui/material/Grid";
+import { Container } from "@mui/material";
+import LinearProgressCustom from "./Components/LinearProgressCustom";
+import QuestionCard from "./Components/QuestionCard";
+import { useState } from "react";
+
+const questions = [
+  {
+    title: "I Value",
+    options: ["Justice", "Mercy"],
+  },
+  {
+    title: "I appreciate a wide variety of music",
+    options: [
+      "Rarely",
+      "Occasionally",
+      "Sometimes",
+      "Usually",
+      "Almost Always",
+    ],
+  },
+  {
+    title: "A quiet weekend at home is",
+    options: ["Boring", "rejuvenating"],
+  },
+  {
+    title: "I prefer speakers that communicate",
+    options: ["literally", "figuratively"],
+  },
+  {
+    title: "With people, I am more often",
+    options: ["brief and to the point", "friendly and warm "],
+  },
+];
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  questions.sort(() => Math.random() - 0.5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <Grid container spacing={2}>
+        <Grid item xs={8}></Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={12}>
+          <LinearProgressCustom progress={progress * 20} />
+        </Grid>
+        <Grid item xs={12}>
+          <QuestionCard
+            questions={questions}
+            progress={progress}
+            setProgress={(value: number) => setProgress(value)}
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,170 @@
+import React, { ChangeEvent, useState } from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
+interface OutlinedCardProps {
+  questions: Array<QuestionValue>;
+  progress: number;
+  setProgress: (value: number) => void;
+}
+
+interface QuestionValue {
+  title: string;
+  options: Array<string>;
+}
+
+const questionsCard = (
+  questions: Array<QuestionValue>,
+  noQuiz: number,
+  setNoQuiz: (noQuiz: number) => void,
+  allAnswer: Array<string>,
+  setAllAnswer: (allAnswer: Array<string>) => void,
+  testDone: boolean,
+  setTestDone: (value: boolean) => void,
+  progress: number,
+  setProgress: (value: number) => void
+) => (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ mb: 1.5 }} variant="h5" component="div">
+        {`${questions[noQuiz].title}`}
+      </Typography>
+
+      <FormControl>
+        <FormLabel id="demo-row-radio-buttons-group-label">Answer</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="demo-row-radio-buttons-group-label"
+          name="row-radio-buttons-group"
+        >
+          {questions[noQuiz].options.map((option, idx) => (
+            <FormControlLabel
+              value={option}
+              checked={!!allAnswer[noQuiz] && allAnswer[noQuiz] === option}
+              control={
+                <Radio
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    const currentAllAnswer = [...allAnswer];
+                    currentAllAnswer.splice(noQuiz, 1, e.target.value);
+                    setAllAnswer(currentAllAnswer);
+                  }}
+                />
+              }
+              label={option}
+            />
+          ))}
+        </RadioGroup>
+      </FormControl>
+    </CardContent>
+    <CardActions>
+      <Button
+        size="small"
+        onClick={() => {
+          setNoQuiz(noQuiz - 1);
+          setProgress(progress - 1);
+        }}
+        disabled={!noQuiz}
+      >
+        Previous
+      </Button>
+      <Button
+        size="small"
+        onClick={() => {
+          setNoQuiz(noQuiz + 1);
+          setProgress(progress + 1);
+        }}
+        disabled={!allAnswer[noQuiz] || noQuiz === questions.length - 1}
+      >
+        Next
+      </Button>
+      <Button
+        size="small"
+        onClick={() => {
+          setTestDone(true);
+          setProgress(progress + 1);
+        }}
+        disabled={!allAnswer[noQuiz] || !(noQuiz === questions.length - 1)}
+      >
+        Done
+      </Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+const resultCard = (
+  questions: Array<QuestionValue>,
+  noQuiz: number,
+  answer: string
+) => (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ mb: 1.5 }} variant="h5" component="div">
+        {`${questions[noQuiz].title}`}
+      </Typography>
+
+      <FormControl>
+        <FormLabel id="demo-row-radio-buttons-group-label">Answer</FormLabel>
+        <Typography sx={{ mb: 1.5 }} variant="h6" component="div">
+          {`${answer}`}
+        </Typography>
+      </FormControl>
+    </CardContent>
+  </React.Fragment>
+);
+
+export default function OutlinedCard(props: OutlinedCardProps) {
+  const { questions, progress, setProgress } = props;
+  const [allAnswer, setAllAnswer] = useState<string[]>([]);
+  const [noQuiz, setNoQuiz] = useState(0);
+  const [testDone, setTestDone] = useState(false);
+
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      {!testDone && (
+        <Card variant="outlined">
+          {questionsCard(
+            questions,
+            noQuiz,
+            (value: number) => setNoQuiz(value),
+            allAnswer,
+            (answers: string[]) => setAllAnswer(answers),
+            testDone,
+            (value: boolean) => setTestDone(value),
+            progress,
+            (value: number) => setProgress(value)
+          )}
+        </Card>
+      )}
+      {testDone &&
+        allAnswer.map((value, idx) => (
+          <Card variant="outlined">{resultCard(questions, idx, value)}</Card>
+        ))}
+      {testDone && (
+        <Card variant="outlined">
+          <CardContent>
+            <Button
+              size="small"
+              onClick={() => {
+                setAllAnswer([]);
+                setNoQuiz(0);
+                setTestDone(false);
+                setProgress(0);
+              }}
+            >
+              Retry
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+    </Box>
+  );
+}
